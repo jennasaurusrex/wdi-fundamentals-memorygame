8f50bfafe195e5aa449d4f87227bb1f6a2e9ddca
unknown <jlnorthcott@gmail.com>
2018-12-04 09:03:50 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,16 +1,21 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[1];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardsInPlay);
-
-if (cardsInPlay.length === 2) {
+var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");
+	alert("You found a match!");
+} 	else {
+	alert("Sorry, try again.");
 	}
 }
+
+var flipcard = function(cardID) {
+	cardsInPlay.push(cards[cardID]);
+	console.log ("User flippped " + cards[cardID]);
+
+	if  (cardsInPlay.length === 2);
+	checkForMatch();
+}
+
+flipcard(0);
+flipcard(2);
